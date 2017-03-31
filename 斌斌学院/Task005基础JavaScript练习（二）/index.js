139c66ade76a57cb6c9d116d86414579ff826c28
RookieDay <1109318841@qq.com>
2017-03-31 22:22:26 +0800

    init

--- 斌斌学院/Task005基础JavaScript练习（二）/index.js
+++ 斌斌学院/Task005基础JavaScript练习（二）/index.js
@@ -19,38 +19,163 @@ QueueElement.prototype = {
         var data = this.data;
         var _this = this;
         var str = "";
-        // data.forEach(function(value,index){
-        //     var node = this._crea
-        // })
+        data.forEach(function(value, index) {
+            var node = this._createElementItem(value);
+            this.element.appendChild('node');
+        })
+        this.registEvent(obj);
     },
     registEvent: function(obj) {
         this.leftInBtn.addEventListener('click', function() {
-            var data = getInputValue(obj.inputContent);
-            obj._unshift(obj.dataContent, data, obj.data);
-        })
-        this.rightInBtn.addEventListener('click', function() {
-            var data = getInputValue(obj.inputContent);
-            obj._push(obj.dataContent, data, obj.data);
-        })
-        this.leftOutBtn.addEventListener('click', function() {
-            var data = getInputValue(obj.inputContent);
-            obj._shift(obj.dataContent, data, obj.data);
-        })
-        this.rightOutBtn.addEventListener('click', function() {
-            var data = getInputValue(obj.inputContent);
-            obj._pop(obj.dataContent, data, obj.data);
-        })
-        this.queueData.addEventListener('click', function(e) {
-            if (e.target && e.target.nodeName.toLowerCase() == 'li') {
+                var data = getInputValue(obj.inputContent);
+                obj._unshift(obj.dataContent, data, obj.data);
+            }),
+            this.rightInBtn.addEventListener('click', function() {
+                var data = getInputValue(obj.inputContent);
+                obj._push(obj.dataContent, data, obj.data);
+            }),
+            this.leftOutBtn.addEventListener('click', function() {
+                var data = getInputValue(obj.inputContent);
+                obj._shift(obj.dataContent, data, obj.data);
+            }),
+            this.rightOutBtn.addEventListener('click', function() {
+                var data = getInputValue(obj.inputContent);
+                obj._pop(obj.dataContent, data, obj.data);
+            }),
+            this.dataContent.addEventListener('click', function(e) {
+                if (e.target && e.target.nodeName.toLowerCase() == 'li') {
+                    var index;
+                    var dataList = this.children;
+                    for (var i = 0; i < dataList.length; i++) {
+                        if (dataList[i] == e.target) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    if (index >= 0) {
+                        // remove array data
+                        obj.data.splice(index, 1);
+                        // remove html 
+                        obj.dataContent.removeChild(e.target);
+                        alert('Removed:' + e.target.innerText + "Loc: " + (++index));
+                    } else {
+                        alert("failed!");
+                    }
+                }
+            }),
+            this.sortBtn.addEventListener('click', function() {
+                obj.data = obj._bubbleSort(obj.data);
+                console.log(obj.data);
 
-            }
-        })
+                obj.dataContent.innerHTML = '';
+                obj.data.forEach(function(value, index) {
+                    var node = obj._createElementItem(value)
+                    obj.element.appendChild(node);
+                });
+            })
+
     },
+    getInputValue: function(element) {
+        var data = Number(element.value);
+        if (!data) {
+            alert('Pls input...')
+            return null;
+        } else {
+            if (data >= 10 && data <= 100) {
+                return data;
+            } else {
+                alert('err range');
+            }
+        }
+    },
+    _unshift: function(element, data, queueData) {
+        if (data) {
+            queueData.unshift(data);
+            if (this._judgeLength(queueData)) {
+                var node = this._createElementItem(data);
+                element.insertBefore(node, element.getElementById('queueData'));
+                return queueData;
+            } else {
+                alert('>60')
+            }
+        }
+        return queueData;
+    },
+    push: function(element, data, queueData) {
+        if (data) {
+            queueData.push(data);
+            if (this._judgeLength(queueData)) {
+                var node = this._createElementItem(data);
+                element.appendChild(node);
+                return queueData;
+            } else {
+                alert('>60')
+            }
+        }
+        return queueData;
+    },
+    _shift: function(element, data, queueData) {
+        if (queueData.length <= 0) {
+            alert('empty list');
+            return queueData;
+        } else {
+            alert('removed: ' + queueData[0]);
+            queueData.shift();
+            element, removeChild(element.children[0]);
+            return queueData;
+        }
+    },
+    _pop: function(element, data_queue) {
+        var length = data_queue.length
+        if (length <= 0) {
+            alert('列表已空，移除失败');
+            console.error('列表已空，移除失败');
+            return data_queue;
+        } else {
 
+            alert('移除的元素内数值为: ' + data_queue[length - 1]);
+            data_queue.pop();
+            element.removeChild(element.children[length - 1]);
+            return data_queue;
+        }
+    },
+    _judgeLength: function(data_queue) {
+        if (data_queue.length > 60) {
+            return false
+        } else {
+            return true;
+        }
+    },
+    _createItemElement: function(data) {
+        var node = document.createElement("div");
+        node.innerText = data;
+        node.setAttribute("data-num", data);
+        node.style.height = data + 'px';
+        return node;
+    },
+    _bubbleSort: function(arr) {
+        var flag = true; //设置标志位
+        var nums = arr.length;
+        for (var i = nums; i >= 2 && flag; i--) {
+            flag = false; //如果一次交换能都没有过，则说明冒泡排序已结束
+            for (var j = 0; j < i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    this.swap(arr, j, j + 1);
+                }
+            }
+        }
+        return arr;
+    },
+    swap: function(arr, index1, index2) {
+        var temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
 }
 
 window.onload = function() {
     var lists = document.getElementById('queueData');
     var data = [];
-    queueInit(list, data);
+    queueInit(lists, data);
 }
